ea4c82cf4e9c22e718c66da97c752856afad9d14
avinnitsky <avinnitsky@exadel.com>
2020-07-18 21:01:07 +0300

    add component factory

---
+++ src/components/JSComponents.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="js-component-wrapper">
+        <input dateStart><input dateEnd>
+        <div componentsList></div>
+    </div>
+</template>
+
+<script>
+    import {compentFactory} from './componentFactory'
+
+    export default {
+        mounted() {
+            const contract = compentFactory(
+                'Contract',
+                $(this.$el.querySelector('[dateStart]')).datepicker(),
+                $(this.$el.querySelector('[dateEnd]')).datepicker(),
+                {defaultDate: new Date()}
+            )
+            $(this.$el.querySelector('[componentsList]')).append(contract)
+        }
+    }
+</script>
+
+<style>
+    div.js-component-wrapper input {
+        border: 1px solid #ccc;
+        padding: 3px;
+        margin: 3px;
+    }
+</style>
